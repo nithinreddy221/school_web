f8cf81c6987b1116f0525c9efc3b63313af3f1e7
malathi <malathic0798@gmail.com>
2025-01-25 10:05:34 +0530

    teachers

--- teachers/admin.py
+++ teachers/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Teacher, TeacherStudentAssignment
 
-# Register your models here.
+
+
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'hire_date', 'salary')
+    list_editable = ('salary',)  # Fields that can be edited directly in the list view
+    search_fields = ('first_name', 'last_name', 'email')  # Add a search bar
+    list_filter = ('hire_date', 'salary')
+
+admin.site.register(Teacher, TeacherAdmin)
+admin.site.register(TeacherStudentAssignment)
